0cc871a673aa7188e434dcb823a65f91ca4d94f0
Mark Aldwin <acdwinwin@gmail.com>
2026-01-11 01:09:34 +0800

    NavBar draft done

--- portfolio2026/src/pages/home/Home.jsx
+++ portfolio2026/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import "./Home.css";
+
 // src/pages/home/Home.jsx
 
 function Home() {
     return (
-        <div>
+        <div className="home">
             <h1>Home Page</h1>
             <p>Welcome to my new project!</p>
         </div>
